16633c1b155c371cc70e859b6b85538e4ba0116c
Darlinx25 <fsalaberry505@gmail.com>
2025-08-17 17:40:35 -0300

    Mosquitto, compose y scripts

---
+++ publicar.sh
@@ -0,0 +1,2 @@
+mosquitto_pub -h localhost -t "habitacion/ambiente" -m "{\"temp_celsius\": 29, \"humedad\": 81}"
+mosquitto_pub -h localhost -t "habitacion/ambiente" -m "{\"temp_celsius\": 30, \"humedad\": 87}"
