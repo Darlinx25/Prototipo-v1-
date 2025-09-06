afd14408cf3f7161b6ee622bfe5a1767ea742e33
Faxcundo <fsalaberry505@gmail.com>
2025-09-06 20:56:34 -0300

    Update consumidor.py

--- consumidor/consumidor.py
+++ consumidor/consumidor.py
@@ -1,16 +1,32 @@
 import json
+import logging
 import paho.mqtt.client as mqtt
 
-def on_message(client, userdata, message): #Función que se ejecuta cada vez que llega un mensaje al topic subscripto
-    msg = message.payload.decode("utf-8") #Decodifica el payload a string y lo convierte de JSON a diccionario
-    try:
-        data = json.loads(msg)
-        print("Recibido:", data)
-    except json.JSONDecodeError:
-        print("Mensaje inválido:", msg) #Imprime el mensaje recibido o error si no es JSON.
+# Configuramos logging
+logging.basicConfig(
+    filename='/app/logs/consumidor.log',  # Archivo donde se guardan los logs
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s'
+)
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, protocol=mqtt.MQTTv311) #Crea un cliente MQTT.
-client.on_message = on_message #Asigna la función on_message para manejar mensajes.
-client.connect("mosquitto", 1883, 60) #Conecta al broker llamado "mosquitto" en el puerto 1883.
-client.subscribe("habitacion/ambiente") #Se suscribe al topic "habitacion/ambiente".
-client.loop_forever() #loop_forever() → Entra en un loop infinito, esperando mensajes y ejecutando on_message.
+def on_message(client, userdata, message):
+    msg = message.payload.decode("utf-8")  # Decodifica el payload
+    try:
+        data = json.loads(msg)  # Intenta convertir a diccionario
+        logging.info(f"Recibido: {data}")  # Guarda log en archivo
+        print("Recibido:", data)           # También imprime en consola
+    except json.JSONDecodeError:
+        logging.warning(f"Mensaje inválido: {msg}")  # Mensaje inválido al log
+        print("Mensaje inválido:", msg)
+
+# Creamos el cliente MQTT
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, protocol=mqtt.MQTTv311)
+client.on_message = on_message
+
+# Conectamos al broker y nos suscribimos al topic
+client.connect("mosquitto", 1883, 60)
+client.subscribe("habitacion/ambiente")
+
+# Loop infinito esperando mensajes
+client.loop_forever()
+
