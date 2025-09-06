b0b586dc7894d0eb9f9d50bd9a82a1ec2bc65a20
Faxcundo <fsalaberry505@gmail.com>
2025-09-06 20:56:50 -0300

    Update sensor.py

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -1,7 +1,15 @@
 import time
 import json
 import random
+import logging
 import paho.mqtt.client as mqtt
+
+# Configuramos logging
+logging.basicConfig(
+    filename='/app/logs/sensor.log',  # Archivo donde se guardan los logs
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s'
+)
 
 # Conectamos al broker usando el nombre del servicio en Compose
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, protocol=mqtt.MQTTv311)
@@ -14,8 +22,13 @@ while True:
         "humedad": random.randint(50, 90)
     }
     payload = json.dumps(msg)
+    
+    # Publicamos el mensaje
     client.publish("habitacion/ambiente", payload)
-    import logging
-    logging.basicConfig(filename='/app/logs/sensor.log', level=logging.INFO)
-    logging.info(payload)
+    
+    # Guardamos log en archivo y consola
+    logging.info(f"Publicado: {payload}")
+    print("Publicado:", payload)
+    
     time.sleep(1)
+
